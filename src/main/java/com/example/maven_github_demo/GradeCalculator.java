65a1a9a75910f224851419a6df32d247f6cdf640
Your Name <vijay.cp@vvce.ac.in>
2026-09-26 15:33:03 +0530

    no error

--- src/main/java/com/example/maven_github_demo/GradeCalculator.java
+++ src/main/java/com/example/maven_github_demo/GradeCalculator.java
@@ -12,7 +12,7 @@ public class GradeCalculator
 		} 
 	public static boolean isPass(double average)
 	{ 
-		return average >= 32.0; 
+		return average >= 40.0; 
 		} 
 	public static void main(String[] args)
 	{ 
